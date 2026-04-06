518a591dfbdde614deddccf31cba7c6a3ed74358
Ilja Nevolin <53948000+inevolin@users.noreply.github.com>
2026-04-06 20:41:33 +0100

    chore: fix ts-jest TS151002 warning with isolatedModules

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/src/**/*.test.ts'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: { isolatedModules: true } }],
+  },
 };
